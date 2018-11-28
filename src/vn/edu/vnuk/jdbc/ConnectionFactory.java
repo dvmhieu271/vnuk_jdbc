2c9b9c215d2b2094c62930dcdcc19f6e45f5009e
dvmhieu271 <hieu.duong160204@vnuk.edu.vn>
2018-11-28 17:26:10 +0700

    Test Connection

--- src/vn/edu/vnuk/jdbc/ConnectionFactory.java
+++ src/vn/edu/vnuk/jdbc/ConnectionFactory.java
@@ -8,9 +8,9 @@ public class ConnectionFactory {
 	public Connection getConnection() {
 		try {
 			return DriverManager.getConnection(
-				"jdbc:mysql://localhost/vnuk_personal",
+				"jdbc:mysql://localhost/vnuk",
 				"root",
-				""
+				"0Pskmhkhbthkbb"
 			);
 		}
 		catch (SQLException e) {
